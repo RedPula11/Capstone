14d6cf04481dade1052871126701fb161575852f
RedPula11 <101171349+RedPula11@users.noreply.github.com>
2025-02-27 16:08:53 +0800

    Add files via upload

---
+++ pages/main.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+st.title("THIS IS THE GREATEST SHOW")
+st.subheader("It is always fun to be here")
+st.write("Join us and earn great salary")
+st.divider()
+st.image("cat.webp","Smile ka naman")
+
+
+
